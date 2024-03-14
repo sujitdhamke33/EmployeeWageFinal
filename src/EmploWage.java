3932178df9c4d20e6054a2464e315059ee72e602
sujitdhamke33 <sujitdhamke12@gmail.com>
2024-03-14 16:44:22 +0530

    Employee Attendence

--- src/EmploWage.java
+++ src/EmploWage.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class EmploWage {
@@ -5,5 +7,16 @@ public class EmploWage {
         System.out.println("Welcome to Employee\n" +
                 "Wage Computation\n" +
                 "Program on Master Branch");
+        int attendence = randomAttendence();
+
+        if(attendence == 1){
+            System.out.println("Employee is Present");
+        }else{
+            System.out.println("Employee is Absent");
+        }
+    }
+    static int randomAttendence(){
+        Random rand = new Random();
+        return rand.nextInt(2);
     }
 }
