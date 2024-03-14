fa1010ca402f834f65afc904a41e70b429259f40
sujitdhamke33 <sujitdhamke12@gmail.com>
2024-03-14 16:39:40 +0530

    Master Created

---
+++ src/EmploWage.java
@@ -0,0 +1,9 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class EmploWage {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee\n" +
+                "Wage Computation\n" +
+                "Program on Master Branch");
+    }
+}
