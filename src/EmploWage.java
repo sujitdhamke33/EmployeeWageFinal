69d351c29b2b3ddacf644db54d4e0027209a2114
sujitdhamke33 <sujitdhamke12@gmail.com>
2024-03-14 21:36:24 +0530

    Using switchCase

--- src/EmploWage.java
+++ src/EmploWage.java
@@ -5,29 +5,36 @@ public class EmploWage {
         int wagePerHour = 20;
         int fullTimeHours = 8;
         int partTimeHours = 4;
-        int attendence = randomAttendence();
 
-        int dailyWage = DailyWage(wagePerHour,fullTimeHours,attendence,partTimeHours);
+        int attendance = generateRandomAttendance();
+        int dailyWage = calculateDailyWage(wagePerHour, attendance, fullTimeHours, partTimeHours);
         System.out.println("Daily Wage: $" + dailyWage);
-
     }
-    static int randomAttendence(){
-        Random rand = new Random();
-        return rand.nextInt(3);
-    }
-    static int DailyWage(int wagePerHour, int fullTimeHours, int attendence,  int partTimeHours) {
 
+
+    private static int generateRandomAttendance() {
+        Random random = new Random();
+        return random.nextInt(3);
+    }
+
+
+    private static int calculateDailyWage(int wagePerHour, int attendance, int fullTimeHours, int partTimeHours) {
         int dailyWage = 0;
 
-        if(attendence == 0){
-            System.out.println("Employee is absent : No daily wage" );
-        }else if(attendence == 1) {
-            dailyWage = wagePerHour * fullTimeHours;
-            System.out.println("Employee is Full time");
-        } else{
-            dailyWage = wagePerHour * partTimeHours;
-            System.out.println("Employee is part_time");
+        switch (attendance) {
+            case 0:
+                System.out.println("Employee is absent.");
+                break;
+            case 1:
+                dailyWage = wagePerHour * fullTimeHours;
+                System.out.println("Full-time employee is present and worked for 8 hours.");
+                break;
+            case 2:
+                dailyWage = wagePerHour * partTimeHours;
+                System.out.println("Part-time employee is present and worked for 4 hours.");
+                break;
         }
+
         return dailyWage;
     }
 }
